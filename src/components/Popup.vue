9ccdcde5356b9c29593f51aa1ddd70715edd9f68
ronald <ronald-farias@outlook.com>
2019-02-13 23:03:18 -0300

    add popup template

---
+++ src/components/Popup.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <Banner />
+    <div
+      class="container">
+      <h1 class="title">{{ manga.name }}, Cap: {{ manga.cap }}</h1>
+      <p>Em: <a :href="manga.origin">{{ manga.origin }}</a></p>
+      <p class="subtitle">Histórico de leitura:</p>
+      <p>atual url: {{ hostname }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue'
+import utils from '../utils'
+
+export default {
+  components: {
+    Banner
+  },
+  data () {
+    return {
+      url: '',
+      manga: {
+        name: '',
+        cap: '',
+        origin: ''  
+      }
+    }
+  },
+  computed: {
+    hostname () {
+      return utils.url_domain(this.url)
+    }
+  },
+  methods: {
+    loadCurrentManga () {
+      this.manga.name = 'One Piece'
+      this.manga.cap = '500'
+      this.manga.origin = 'https://op-ex.com.br'
+    }
+  },
+  mounted () {
+    this.loadCurrentManga()
+
+    utils.current_url().then((resp) => {
+      this.url = resp
+    })
+  }
+}
+</script>
+
+<style scoped lang="css">
+.container {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: rgb(47, 47, 47);
+  text-shadow: 1px 1px 1px rgb(180, 180, 180);
+  padding: 15px;
+  text-align: center;
+}
+
+</style>
