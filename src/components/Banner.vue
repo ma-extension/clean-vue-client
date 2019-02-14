ee6445a5bd45eed38ca0b72da55dcbc938c72ccf
ronald <ronald-farias@outlook.com>
2019-02-13 23:03:42 -0300

    add banner template

---
+++ src/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="banner">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        image_dir: ''
+    }
+  },
+  mounted () {
+    var banner = document.querySelector('.banner')
+    this.image_dir = 'https://sm.ign.com/ign_br/screenshot/default/we-can_213n.jpg'
+  banner.style.backgroundImage = `url('${this.image_dir}')`
+  } 
+}
+</script>
+
+<style scoped lang="css">
+.banner {
+  height: 80px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
