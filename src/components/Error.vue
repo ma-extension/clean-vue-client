60a1cf6e36cca5eec8d087d32fefdb51d1ff97b5
ronald <ronald-farias@outlook.com>
2019-02-13 23:03:48 -0300

    add error template

---
+++ src/components/Error.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Oops, este site não é suportado como um leitor gerenciavel.</h1>
+        <p></p>
+        <h3>Envie uma issue para: github.com</h3>
+        <h3>Ou, atualize sua extensão, quem sabe já não a corrigimos.</h3>
+        <p>url: {{ hostname }}</p>
+    </div>
+</template>
+
+<script>
+import utils from '../utils'
+export default {
+  data () {
+    return {
+      hostname: ''
+    }
+  },
+  mounted () {
+    utils.current_url().then((resp) => {
+      this.hostname = resp
+    })
+  }
+}
+</script>
+
+<style scoped lang="css">
+
+</style>
+
