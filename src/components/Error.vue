9d65d0fc4c8ca37cb7032efa01428e54868a080e
ronald <ronald-farias@outlook.com>
2019-02-20 11:32:08 -0300

    display history from readers

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>
-        <h1>Oops, este site não é suportado como um leitor gerenciavel.</h1>
-        <p></p>
-        <h3>Envie uma issue para: github.com</h3>
-        <h3>Ou, atualize sua extensão, quem sabe já não a corrigimos.</h3>
-        <p>url: {{ hostname }}</p>
+    <div class="container">
+        <div class="title">
+          <h1>Manga Agenda</h1>
+        </div>
+        <div class="readers">
+          <h4>Leitores recentes:</h4>
+          <div v-for="(el, index) in items" :key="index">
+            <p class="readers__name"><strong>{{ el.reader }}:</strong></p>
+            <div v-for="(el_manga, index_mangas) in el.mangas" :key="index_mangas">
+              <p>&nbsp;&nbsp;<strong>{{ el_manga.name }}</strong>
+                <br>
+                <a v-for="(el_caps, index_caps) in el_manga.history" :key="index_caps">
+                  &nbsp;&nbsp;&nbsp;&nbsp;<strong>Cap:</strong> {{ el_caps.cap }}
+                  <br>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="help">
+          <p>Oops, este site não é suportado como um leitor gerenciavel.</p>
+          <a href="#">Ajuda</a>
+        </div>
     </div>
 </template>
 
@@ -13,18 +31,53 @@ import utils from '../utils'
 export default {
   data () {
     return {
-      hostname: ''
+      items: null
     }
   },
   mounted () {
-    utils.current_url().then((resp) => {
-      this.hostname = resp
+    this.getLastReaders().then(resp => {
+      this.items = resp
     })
+  },
+  methods: {
+    getLastReaders () {
+      return new Promise((resolve, reject) => {
+        chrome.storage.sync.get(['historyReaders'], function(items) {
+          resolve(items.historyReaders)
+        })
+      })
+    }
   }
 }
 </script>
 
 <style scoped lang="css">
+.container {
+  /* background configs */
+  /* background-image: url('../assets/galaxy_background.jpg'); */
+  background: rgba(0, 0, 0, 0.8);
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
 
+.container {
+  padding: 16px;
+}
+
+.title {
+  font-family: 'Courier New', Courier, monospace;
+  text-align: center;
+  text-shadow: 1px 1px 5px black;
+  color: aliceblue;
+}
+
+.help {
+  color: aliceblue;
+}
+
+.readers {
+  color: aliceblue;
+}
 </style>
 
