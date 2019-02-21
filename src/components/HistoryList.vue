bedde356475f1b15160824f54aac6e9b1a333982
ronald <ronald-farias@outlook.com>
2019-02-21 20:08:50 -0300

    add windows style based

--- src/components/HistoryList.vue
+++ src/components/HistoryList.vue
@@ -1,17 +1,67 @@
 <template>
     <div class="readers">
-        <h4>Leitores recentes:</h4>
-        <div v-for="(el, index) in items" :key="index">
-        <p class="readers__name"><strong><a :href="el.hostname">{{ el.reader }}:</a></strong></p>
-        <div class="readers__manga" v-for="(el_manga, index_mangas) in el.mangas" :key="index_mangas">
-            <p>&nbsp;&nbsp;<strong>{{ el_manga.name }}</strong>
-            <br>
-            <a class="readers__manga__cap" v-for="(el_caps, index_caps) in el_manga.history" :key="index_caps" :href="el_caps.url">
-                &nbsp;&nbsp;&nbsp;&nbsp;<strong>Cap:</strong> {{ el_caps.cap }}
-                <br>
-            </a>
-            </p>
+        <h4>Leituras recentes:</h4>
+        <!-- <div class="reader">
+            <p class="reader__name">central-de-mangas</p>
+            <div class="reader__manga">
+                <div class="reader__manga__name">
+                    <strong>One Piece</strong>
+                </div>
+                <div class="reader__manga__cap">
+                    <a href="">Cap: 900</a>
+                    <div>
+                        <img class="reader__manga__cap__button" @mousedown="(e) => onDeleteButton(e)" @mouseup="e => onDeleteButton(e)" 
+                        src="../assets/baseline-delete-24px.svg" alt="thrash button">
+                    </div>
+                </div>
+                <div class="reader__manga__cap">
+                    <a href="">Cap: 899</a>
+                    <div>
+                        <img class="reader__manga__cap__button" @mousedown="(e) => onDeleteButton(e)" @mouseup="e => onDeleteButton(e)" 
+                        src="../assets/baseline-delete-24px.svg" alt="thrash button">
+                    </div>
+                </div>
+            </div>
         </div>
+        <div class="reader">
+            <p class="reader__name" @click="onShowReaderMangas()">central-de-mangas</p>
+            <div class="reader__manga">
+                <div class="reader__manga__name">
+                    <strong>One Piece</strong>
+                </div>
+                <div class="reader__manga__cap">
+                    <a href="">Cap: 900</a>
+                    <div>
+                        <img class="reader__manga__cap__button" @mousedown="(e) => onDeleteButton(e)" @mouseup="e => onDeleteButton(e)" 
+                        src="../assets/baseline-delete-24px.svg" alt="thrash button">
+                    </div>
+                </div>
+                <div class="reader__manga__cap">
+                    <a href="">Cap: 899</a>
+                    <div>
+                        <img class="reader__manga__cap__button" @mousedown="(e) => onDeleteButton(e)" @mouseup="e => onDeleteButton(e)" 
+                        src="../assets/baseline-delete-24px.svg" alt="thrash button">
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="reader">
+            <div class="reader__item" v-for="(el, index) in items" :key="index">
+                <p class="reader__name"><strong><a :href="el.hostname">{{ el.reader }}:</a></strong></p>
+                <div class="reader__manga" v-for="(el_manga, index_mangas) in el.mangas" :key="index_mangas">
+                    <div class="reader__manga__name">
+                        <strong>{{ el_manga.name }}</strong>
+                    </div>
+                    <div class="reader__manga__cap" v-for="(el_caps, index_caps) in el_manga.history" :key="index_caps">
+                        <a :href="el_caps.url">{{ el_caps.cap }}</a>
+                        <div>
+                            <img
+                                class="reader__manga__cap__button" @mousedown="(e) => onDeleteButton(e)" @mouseup="e => onDeleteButton(e)" 
+                                src="../assets/baseline-delete-24px.svg" alt="thrash button">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -20,7 +70,8 @@
 export default {
   data () {
     return {
-      items: null
+      items: null,
+      showReaderMangas: ''
     }
   },
   mounted () {
@@ -35,14 +86,95 @@ export default {
           resolve(items.historyReaders)
         })
       })
+    },
+    onDeleteButton (element) {
+        let border = element.target.style.border
+        if (element.type === 'mousedown') {
+            element.target.style.borderTop='none'
+            element.target.style.borderLeft='none'    
+            element.target.style.marginTop = '0px'
+            element.target.style.backgroundColor = 'rgb(0,0,0,0.3)'
+        } else if (element.type === 'mouseup') {
+            element.target.style.backgroundColor = 'transparent'
+            element.target.style.marginTop = '-1px'
+            element.target.style.border=border
+        }
     }
   }
 }
 </script>
 
-<style scoped lang="css">
+<style>
 .readers {
-  color: aliceblue;
+    /* margin: 5px; */
+    /* background: #4C4A48;  */
+  /* color: azure; */
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 14px;
+}
+.reader {
+  /* box-shadow: 0px 2px 5px rgba(59, 59, 59, 0.342); */
+}
+
+.reader__item {
+    margin-bottom: 10px;
+    background: rgba(128, 128, 128, 0.205);
+}
+
+.reader__name:hover {
+    background: rgba(128, 128, 128, 0.205);
+}
+.reader__name {
+    background: rgba(128, 128, 128, 0.205);
+    font-size: 16px;
+    font-weight: bold;
+    padding: 15px 0px 15px 0px;
+
+    text-decoration: none;
+    color: currentColor;
+    text-align: center;
+}
+.reader__name a {
+    text-decoration: none;
+    color: currentColor;
+}
+.reader__manga {
+    padding: 5px;
+}
+.reader__manga__name {
+    padding: 5px;
+    border-bottom: 1px solid rgba(59, 59, 59, 0.335);
+}
+.reader__manga__name:hover {
+    background: rgba(128, 128, 128, 0.205);
+}
+
+.reader__manga__cap {
+    text-decoration: none;
+    padding: 5px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+}
+
+.reader__manga__cap a{
+    flex: 1;
+    text-decoration: none;
+    color: currentColor;
+    width: 100px;
+
+    transition: 0.5s ease;
+}
+
+.reader__manga__cap:hover {
+    /* color: azure; */
+    background-color: #3DB9CC;
+    font-size: 15px;
+}
+
+.reader__manga__cap__button:hover {
+    margin: -1px;
+    border: 1px solid rgba(255, 255, 255, 0.486);
 }
 .readers__manga__cap {
   text-decoration: none;
