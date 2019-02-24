1d572731fe9894a3e652dc0051f87817b98c2133
ronald <ronald-farias@outlook.com>
2019-02-23 22:40:40 -0300

    remove unecessary code and add title and backkground color

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <Banner />
+    <!-- <Banner /> -->
     <div
       class="container">
-      <p>Você parou em:</p>
-      <h1 class="title">{{ reader.last_manga.name }}, Cap: {{ reader.last_manga.cap }}</h1>
-      <p>Em: <a :href="reader.last_manga.url" @click.native="gotoRoot()">{{ reader.last_manga.url }}</a></p>
-      <p class="subtitle">Histórico de leitura:</p>
-      <div v-for="(current, index) in lastMangaHistory()" :key="index">
-        <p>{{ reader.last_manga.name}}, {{ current.cap }}</p>
+      <div class="popup__title">
+        <p>Recentemente lidos</p>
       </div>
+      <HistoryList showHistoryFrom="current"/>
     </div>
   </div>
 </template>
@@ -17,61 +14,33 @@
 <script>
 import Banner from '../components/Banner.vue'
 import utils from '../utils'
-import history from '../history.json'
-
+import HistoryList from '../components/HistoryList.vue'
 export default {
   components: {
+    HistoryList,
     Banner
   },
   data () {
     return {
       url: '',
-      reader: null
+      hostname: null
     }
-  },
-  computed: {
-    hostname () {
-      return utils.url_domain(this.url)
-    },
-    // reader () {
-    //   utils.current_reader().then(resp => {
-    //     return resp
-    //   }).catch(err => {
-    //     console.log(err)
-    //   })
-    // }
-  },
-  methods: {
-    gotoRoot () {
-      chrome.tabs.update(null, {url: 'https://'+this.hostname})
-    },
-    lastMangaHistory () {
-      return this.reader.mangas.filter((element) => {
-        return element.name === this.reader.last_manga.name
-      })[0].history
-    }
-  },
-  mounted () {
-    utils.current_reader().then(resp => {
-        this.reader = resp
-      }).catch(err => {
-        console.log(err)
-      })
-
-    utils.current_url().then((url) => {
-      this.url = url
-    })
   }
 }
 </script>
 
 <style scoped lang="css">
+.popup__title {
+  font-weight: bold;
+  margin-bottom: 15px;
+  /* color: rgb(8, 131, 125); */
+  font-style: italic;
+}
 .container {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: rgb(47, 47, 47);
-  text-shadow: 1px 1px 1px rgb(180, 180, 180);
   padding: 15px;
   text-align: center;
+  background: rgb(238, 238, 238);
 }
 
 </style>
